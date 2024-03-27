75c1aa18888591002d89f7f3b13ae88915ad4496
Kevin Kipper <kevinkippervfx@socal.rr.com>
2024-03-27 07:55:13 -0700

    Fixed label, added more heading buttons

--- tg_sunlight01_heading_elevation.py
+++ tg_sunlight01_heading_elevation.py
@@ -3,10 +3,10 @@ import terragen_rpc as tg
 
 gui = Tk()
 gui.title("tg_sunlight01_heading_elevation")
-gui.geometry("450x260")
+gui.geometry("460x320")
 
-frame1 = LabelFrame(gui,text="Set Sunlight01 heading in degrees ",padx=37,pady=10)
-frame2 = LabelFrame(gui,text="Offset XZ clipboard coordinates with Transform shader node",padx=5,pady=5)
+frame1 = LabelFrame(gui,text="Set Sunlight01 heading in degrees ",padx=22,pady=10)
+frame2 = LabelFrame(gui,text="Set Sunlight01 elevation in degrees",padx=5,pady=5)
 frame3 = LabelFrame(gui,relief=FLAT,bg="#FFF9EC") # Errors and messages
 
 frame1.grid(row=1,column = 0,padx=5)
@@ -88,14 +88,27 @@ sun_elevation = IntVar()
 operation_message = StringVar()
 
 # Sunlight heading section
-button1 = Button(frame1,text="0",command=lambda: set_sun_heading(0)).grid(row=0,column=0,padx=10,pady=5)
-button2 = Button(frame1,text="45",command=lambda: set_sun_heading(45)).grid(row=0,column=1,padx=10,pady=5)
-button3 = Button(frame1,text="60",command=lambda: set_sun_heading(60)).grid(row=0,column=2,padx=10,pady=5)
-button4 = Button(frame1,text="90",command=lambda: set_sun_heading(90)).grid(row=0,column=3,padx=10,pady=5)
-button5 = Button(frame1,text="130",bg='cyan',command=lambda: set_sun_heading(130)).grid(row=0,column=4,padx=10,pady=5)
-button6 = Button(frame1,text="180",command=lambda: set_sun_heading(180)).grid(row=0,column=5,padx=10,pady=5)
-button7 = Button(frame1,text="270",command=lambda: set_sun_heading(270)).grid(row=0,column=6,padx=10,pady=5)
-button8 = Button(frame1,text="300",command=lambda: set_sun_heading(300)).grid(row=0,column=7,padx=10,pady=5)
+button1 = Button(frame1,text="0",command=lambda: set_sun_heading(0)).grid(row=0,column=0,padx=5,pady=5)
+button2 = Button(frame1,text="15",command=lambda: set_sun_heading(15)).grid(row=0,column=1,padx=5,pady=5)
+button3 = Button(frame1,text="22.5",command=lambda: set_sun_heading(22.5)).grid(row=0,column=2,padx=5,pady=5)
+button4 = Button(frame1,text="45",command=lambda: set_sun_heading(45)).grid(row=0,column=3,padx=5,pady=5)
+button5 = Button(frame1,text="60",command=lambda: set_sun_heading(60)).grid(row=0,column=4,padx=5,pady=5)
+button6 = Button(frame1,text="75",command=lambda: set_sun_heading(75)).grid(row=0,column=5,padx=5,pady=5)
+button7 = Button(frame1,text="90",command=lambda: set_sun_heading(90)).grid(row=0,column=6,padx=5,pady=5)
+button8 = Button(frame1,text="115",command=lambda: set_sun_heading(115)).grid(row=0,column=7,padx=5,pady=5)
+button9 = Button(frame1,text="130",bg="cyan",command=lambda: set_sun_heading(130)).grid(row=0,column=8,padx=5,pady=5)
+button10 = Button(frame1,text="145",command=lambda: set_sun_heading(145)).grid(row=0,column=9,padx=5,pady=5)
+
+button11 = Button(frame1,text="160",command=lambda: set_sun_heading(160)).grid(row=1,column=0,padx=5,pady=5)
+button12 = Button(frame1,text="180",command=lambda: set_sun_heading(180)).grid(row=1,column=1,padx=5,pady=5)
+button13 = Button(frame1,text="195",command=lambda: set_sun_heading(195)).grid(row=1,column=2,padx=5,pady=5)
+button14 = Button(frame1,text="205",command=lambda: set_sun_heading(205)).grid(row=1,column=3,padx=5,pady=5)
+button15 = Button(frame1,text="240",command=lambda: set_sun_heading(240)).grid(row=1,column=4,padx=5,pady=5)
+button16 = Button(frame1,text="270",command=lambda: set_sun_heading(270)).grid(row=1,column=5,padx=5,pady=5)
+button17 = Button(frame1,text="300",command=lambda: set_sun_heading(300)).grid(row=1,column=6,padx=5,pady=5)
+button18 = Button(frame1,text="315",command=lambda: set_sun_heading(315)).grid(row=1,column=7,padx=5,pady=5)
+button19 = Button(frame1,text="330",command=lambda: set_sun_heading(330)).grid(row=1,column=8,padx=5,pady=5)
+
 
 # Sunlight elevation section
 slider1 = Scale(frame2,from_= -90, to = 90, variable=sun_elevation,orient=HORIZONTAL,showvalue=0,length=360).grid(row=0,column=0,padx=10,pady=5)
